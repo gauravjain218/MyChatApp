d43ffbd7f9dbb4750bacf77569c0a00d3b77d035
gauravjain218 <gaurav.ujn@rediffmail.com>
2019-03-12 22:50:35 +0530

    create new app

---
+++ socket-server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+
+var app = express();
+
+const port=process.env.PORT || 3000
+
+var http = require("http");
+
+var server = http.createServer(app);
+
+var io = require("socket.io")(server);
+
+io.on("connection", client =>{
+    console.log("New client connected...", client.id);
+    client.emit("acknowledge", {data : "Connected"});
+
+    client.on("msgToServer", (chatterName, msg) => {
+        console.log(chatterName + " says : " + msg);
+        client.broadcast.emit("msgToClient", chatterName , msg);
+        client.emit("msgToClient", 'Me', msg);
+    })
+
+    client.on("disconnect", ()=>{
+        console.log("Client disconnected." + client.id);
+        //Save data into DB
+    })
+
+})
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + '/public/socket-client.html');
+})
+
+// server.listen(3000, ()=>{
+//     console.log("Socket server running on port 3000");
+// })
+
+server.listen(port,() => {
+    console.log(`Server running at port `+port)});
